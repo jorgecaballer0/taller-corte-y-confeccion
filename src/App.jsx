4ffcf29f0f4eb4d39733e1b2467254e37ad41df3
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2024-06-23 14:49:16 -0300

    Base proyecto taller de corte y confeccion

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Taller de corte y confeccion</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
